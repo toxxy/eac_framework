940a597f1ea12d1561d363c1d5e044cef4cc3e0e
Emmanuel Cardenaz <cardenaz_1000@hotmail.com>
2017-10-13 10:07:48 -0500

    Add files via upload

---
+++ eac_dataGridView.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace EAC_Framework
+{
+    class eac_dataGridView
+    {
+        DataGridView gridview;
+        eac_sqlConnector sqlConn;
+        bool enable_autoUpdate;
+        string tableName;
+
+        public eac_dataGridView(ref DataGridView gridviews)
+        {
+            enable_autoUpdate = true;
+            gridview = gridviews; // Usig pointer for edit in form 
+            //For event CellClick
+            gridview.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(CellClickTest);
+            gridview.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(CellEndEditUpdateGrid);
+            gridview.CellValueChanged += //It is for Paste from clipboard
+                new System.Windows.Forms.DataGridViewCellEventHandler(CellEndEditUpdateGrid); 
+            gridview.KeyDown += new System.Windows.Forms.KeyEventHandler(copyPaste_KeyDown);
+        }
+
+        public void fillDataGridTest()
+        {
+            gridview.ColumnCount = 1;
+            gridview.Columns[0].Name = "Prueba";
+            gridview.Rows.Add("Hola");
+        }
+
+        public void fillGridFromSqlSelect(string QuerySelect, ref eac_sqlConnector sqlConnector)
+        {
+            sqlConn = sqlConnector;
+            gridview.DataSource = null;
+            gridview.Rows.Clear();
+            DataTable result = sqlConn.Select(QuerySelect);
+            gridview.DataSource = result;
+            tableName = result.TableName;
+            //MessageBox.Show(result.ToString());
+        }
+
+        private void CellClickTest(object sender, DataGridViewCellEventArgs e)
+        {
+            //   MessageBox.Show("Works here");
+        }
+
+        private void CellEndEditUpdateGrid(object sender, DataGridViewCellEventArgs e)
+        {
+            string queryUpdate = "UPDATE " + tableName;
+            if (enable_autoUpdate)
+            {
+                int idx = gridview.CurrentCell.ColumnIndex;
+                DataGridViewRow actualRow = gridview.Rows[gridview.CurrentCell.RowIndex];
+                List<string> sqlFields = new List<string>();
+                queryUpdate += " SET [" + gridview.Columns[idx].Name + "] = '" +
+                    actualRow.Cells[idx].Value.ToString() + "' WHERE ";
+
+                for (int i = 0; i < gridview.Columns.Count; i++)
+                {
+                    if (i != idx)
+                    {
+                        sqlFields.Add(" [" + gridview.Columns[i].Name + "] = '" +
+                            actualRow.Cells[i].Value.ToString() + "'");
+                    }
+
+                }
+                queryUpdate += string.Join(" AND", sqlFields.ToArray());
+                sqlConn.Update(queryUpdate);
+                //                MessageBox.Show(queryUpdate);
+            }
+        }
+
+        private void CopyToClipboard(object sender)
+        {
+            DataObject dataObj = gridview.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+
+        private void PasteClipboard(object sender)
+        {   
+            //This code (Copy Paste) is no my own.  But i no remember the Blog, i will reference this... 
+            try
+            {
+                string s = Clipboard.GetText();
+                string[] lines = s.Split('\n');
+
+                int iRow = gridview.CurrentCell.RowIndex;
+                int iCol = gridview.CurrentCell.ColumnIndex;
+                DataGridViewCell oCell;
+                if (iRow + lines.Length > gridview.Rows.Count - 1)
+                {
+                    bool bFlag = false;
+                    foreach (string sEmpty in lines)
+                    {
+                        if (sEmpty == "")
+                        {
+                            bFlag = true;
+                        }
+                    }
+                    int iNewRows = iRow + lines.Length - gridview.Rows.Count;
+                    if (iNewRows > 0)
+                    {
+                        if (bFlag)
+                            gridview.Rows.Add(iNewRows);
+                        else
+                            gridview.Rows.Add(iNewRows + 1);
+                    }
+                    else
+                        gridview.Rows.Add(iNewRows + 1);
+                }
+                foreach (string line in lines)
+                {
+                    if (iRow < gridview.RowCount && line.Length > 0)
+                    {
+                        string[] sCells = line.Split('\t');
+                        for (int i = 0; i < sCells.GetLength(0); ++i)
+                        {
+                            if (iCol + i < gridview.ColumnCount)
+                            {
+                                oCell = gridview[iCol + i, iRow];
+                                oCell.Value = Convert.ChangeType(sCells[i].Replace("\r", ""), oCell.ValueType);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        iRow++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Clipboard.Clear();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("The data you pasted is in the wrong format for the cell");
+                return;
+            }
+        }
+
+        private void copyPaste_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Modifiers == Keys.Control)
+                {
+                    switch (e.KeyCode)
+                    {
+                        case Keys.C:
+                            CopyToClipboard(sender);
+                            break;
+
+                        case Keys.V:
+                            PasteClipboard(sender);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Copy/paste operation failed. " + ex.Message, "Copy/Paste", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+}
---
+++ eac_sqlConnector.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace EAC_Framework
+{
+    class eac_sqlConnector
+    {
+        public string stringConnection;
+        public SqlConnection conn;
+
+        public eac_sqlConnector(string serverName, string dataBaseName, 
+            string table = "", string user = "", string password = "")
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString =
+            "Data Source=" + serverName + ";" +
+            "Initial Catalog=" + dataBaseName + ";" +
+            "User id=" + user + ";" +
+            "Password=" + password + ";";
+        }
+
+        public void Update(string query)
+        {
+            conn.Open();
+            SqlCommand exec = new SqlCommand(query, conn);
+            exec.ExecuteNonQuery();
+            conn.Close();
+        }
+        public DataTable Select(string query)
+        {
+            DataTable result = new DataTable();
+            SqlDataReader res;
+            conn.Open();
+            SqlCommand exec = new SqlCommand(query, conn);
+            try
+            {
+                res = exec.ExecuteReader();
+                result.Load(res);
+            }
+            catch (Exception exs)
+            {
+                MessageBox.Show(exs.ToString());
+            }
+            conn.Close();
+
+            string tablename = GetWordAfter("from", query);
+            result.TableName = tablename;
+            return result;
+        }
+        private void Open(SqlConnection conn)
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception exep)
+            {
+                MessageBox.Show("" + exep.ToString());
+            }
+        }
+        private void Close(SqlConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (Exception exep)
+            {
+                MessageBox.Show("" + exep.ToString());
+            }
+        }
+        static string GetWordAfter(string word, string phrase)
+        {
+            //https://stackoverflow.com/questions/14395721/regex-to-find-the-word-immediately-after-a-particular-word 
+            var pattern = @"\b" + Regex.Escape(word) + @"\s+(\w+)";
+            return Regex.Match(phrase, pattern, RegexOptions.IgnoreCase).Groups[1].Value;
+        }
+    }
+}
